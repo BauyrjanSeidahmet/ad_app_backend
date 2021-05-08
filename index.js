b56c760a464a4a733f2c4eb36f657d4f8087f106
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-05-08 12:07:49 +0600

    #1 сущность категории создана

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./config");
 const app = express();
-
 const port = 8000;
+
+const categories = require("./app/categories");
 
 const run = async () => {
   await mongoose.connect(config.getDbUrl(), {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
@@ -12,6 +13,7 @@ const run = async () => {
   app.use(cors());
   app.use(express.static("public"));
   app.use(express.json());
+  app.use("/categories", categories());
 
   app.listen(port, () => {
     console.log("Server started at http://localhost:" + port);
