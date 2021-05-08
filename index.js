394e4458ab7f4c73eb923ef379a7af7cd1e43a2e
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-05-08 12:27:30 +0600

    #2 сущность пользователей создана

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 8000;
 
 const categories = require("./app/categories");
+const users = require("./app/users");
 
 const run = async () => {
   await mongoose.connect(config.getDbUrl(), {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
@@ -14,6 +15,7 @@ const run = async () => {
   app.use(express.static("public"));
   app.use(express.json());
   app.use("/categories", categories());
+  app.use("/users", users());
 
   app.listen(port, () => {
     console.log("Server started at http://localhost:" + port);
