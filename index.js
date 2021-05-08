9de8d5fed6dac3fb1bf18d682b475cae3e26c8cb
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-05-08 11:34:50 +0600

    начальный коммит

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config");
+const app = express();
+
+const port = 8000;
+
+const run = async () => {
+  await mongoose.connect(config.getDbUrl(), {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+
+  app.use(cors());
+  app.use(express.static("public"));
+  app.use(express.json());
+
+  app.listen(port, () => {
+    console.log("Server started at http://localhost:" + port);
+  });
+};
+
+run().catch(e => console.log(e));
